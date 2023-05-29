577d4c67e3eee736c2ea04fd07d120908115581b
anilj <buduru81anil@gmail.com>
2023-05-29 10:01:23 +0000

    module exports

---
+++ calculator.js
@@ -0,0 +1,4 @@
+const StudentDetails = require("./index");
+const student1 = new StudentDetails("Anil", 23);
+console.log(student1);
+student1.state();
---
+++ index.js
@@ -0,0 +1,12 @@
+class StudentDetails {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  state() {
+    console.log("Himalaya");
+  }
+}
+
+module.exports = StudentDetails;
